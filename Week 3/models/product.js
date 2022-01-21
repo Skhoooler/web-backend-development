328c46abfc75511d41493ad02894a7aa9d6b76be
david doria <dor15004@byui.edu>
2022-01-21 10:43:53 -0700

    updated dependencies

--- Week 3/models/product.js
+++ Week 3/models/product.js
@@ -1,37 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
-);
-
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
+module.exports = class Product{
+    constructor(title) {
+        this.title = title;
     }
-  });
-};
 
-module.exports = class Product {
-  constructor(t) {
-    this.title = t;
-  }
+    save() {
+        const p = path.dirname(process.m)
+    }
 
-  save() {
-    getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
-    });
-  }
-
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
-  }
-};
+    static fetchAll() {
+        return products;
+    }
+}
